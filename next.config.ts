72705bb53e9eee5db6a10d444094255cf0c1923a
greg5320 <art3m43@gmail.com>
2025-04-18 19:12:20 +0300

    feature: add dockerfile and changed config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,19 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
+const nextConfig: NextConfig = {
+  output: 'standalone',
   async rewrites() {
     return [
       {
         source: "/api/clients",
-        destination: "http://localhost:8000/api/clients/",
+        destination: process.env.BACKEND_URL || "http://backend:8000/api/clients/",
       },
     ]
   },
-}
+  
+  images: {
+    domains: ['s3-alpha-sig.figma.com', 'localhost'],
+  },
+};
 
 export default nextConfig;
